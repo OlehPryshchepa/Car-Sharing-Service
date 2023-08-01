8b75eb8d603936079208a8a91b4cea3f93614c46
eugenetolkunov <yevhentolkunov@gmail.com>
2023-08-01 15:49:05 +0300

    [CS-19] Fix field role in user entity, and change field name in car entity (dailyFee)

--- src/main/java/com/example/carsharingservice/model/User.java
+++ src/main/java/com/example/carsharingservice/model/User.java
@@ -1,13 +1,17 @@
 package com.example.carsharingservice.model;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import java.util.Set;
 import lombok.Data;
 
 @Data
@@ -25,9 +29,10 @@ public class User {
     private String lastName;
     @Column(nullable = false)
     private String password;
+    @ElementCollection
+    @CollectionTable(name = "users_roles", joinColumns = @JoinColumn(name = "users_id"))
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+    private Set<Role> roles;
 
     public enum Role {
         MANAGER,
