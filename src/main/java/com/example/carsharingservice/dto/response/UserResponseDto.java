aef5e9ffbf2ee695c3ceddc5b0b125d91d7c80a1
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-01 22:39:46 +0300

    [CS-11] Fixed UserResponseDto trouble with roles

--- src/main/java/com/example/carsharingservice/dto/response/UserResponseDto.java
+++ src/main/java/com/example/carsharingservice/dto/response/UserResponseDto.java
@@ -1,11 +1,13 @@
 package com.example.carsharingservice.dto.response;
 
 import java.util.Set;
+import lombok.Data;
 
+@Data
 public class UserResponseDto {
     private Long id;
     private String email;
     private String firstName;
     private String lastName;
-    private Set<Long> rolesIds;
+    private String role;
 }
