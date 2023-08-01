f48518c9183c67f29853d70d8d31fc595008497e
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-08-01 13:13:41 +0300

    [CS-8]-create next repository: CarRepository, PaymentRepository, RentalRepository, UserReepository

---
+++ src/main/java/com/example/carsharingservice/repository/RentalRepository.java
@@ -0,0 +1,17 @@
+package com.example.carsharingservice.repository;
+
+import com.example.carsharingservice.model.Rental;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    List<Rental> findAllByUser_Id(Long userId, PageRequest pageRequest);
+
+    List<Rental> findByActualReturnDateIsNullAndReturnDateLessThan(LocalDateTime date);
+}
