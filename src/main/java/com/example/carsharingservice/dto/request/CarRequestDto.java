85c030c45769d688082b0185d15936c5002d16c8
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-01 15:30:07 +0300

    [CS-22] Fixed mistakes with dto classes

--- src/main/java/com/example/carsharingservice/dto/request/CarRequestDto.java
+++ src/main/java/com/example/carsharingservice/dto/request/CarRequestDto.java
@@ -11,44 +11,4 @@ public class CarRequestDto {
     private Car.CarType type;
     private int inventory;
     private BigDecimal dailyFree;
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public Car.CarType getType() {
-        return type;
-    }
-
-    public void setType(Car.CarType type) {
-        this.type = type;
-    }
-
-    public int getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(int inventory) {
-        this.inventory = inventory;
-    }
-
-    public BigDecimal getDailyFree() {
-        return dailyFree;
-    }
-
-    public void setDailyFree(BigDecimal dailyFree) {
-        this.dailyFree = dailyFree;
-    }
 }
