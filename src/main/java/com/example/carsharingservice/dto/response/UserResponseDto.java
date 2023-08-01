c771c730812b7fb8a09c807628af48beb7e799e3
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-01 13:17:26 +0300

    [CS-17]-Created DTO for models User, Car, Rental

---
+++ src/main/java/com/example/carsharingservice/dto/response/UserResponseDto.java
@@ -0,0 +1,51 @@
+package com.example.carsharingservice.dto.response;
+
+import java.util.Set;
+
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Set<Long> rolesIds;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Long> getRolesIds() {
+        return rolesIds;
+    }
+
+    public void setRolesIds(Set<Long> rolesIds) {
+        this.rolesIds = rolesIds;
+    }
+}
