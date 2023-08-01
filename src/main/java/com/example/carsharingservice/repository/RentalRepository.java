6c6f1eaadacea8310fe65b28c7e5b9e73e0a3331
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-08-01 15:38:18 +0300

    [CS-8] fix checkstyle mistakes in RentalRepository

--- src/main/java/com/example/carsharingservice/repository/RentalRepository.java
+++ src/main/java/com/example/carsharingservice/repository/RentalRepository.java
@@ -1,9 +1,7 @@
 package com.example.carsharingservice.repository;
 
 import com.example.carsharingservice.model.Rental;
-import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
