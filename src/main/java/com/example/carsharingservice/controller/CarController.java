d53c6bbb06fe6916171ca8a0fd35477c9eab633d
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-08-02 18:15:53 +0300

    "[CS-12] completed CarController"

--- src/main/java/com/example/carsharingservice/controller/CarController.java
+++ src/main/java/com/example/carsharingservice/controller/CarController.java
@@ -1,40 +1,59 @@
 package com.example.carsharingservice.controller;
 
+import com.example.carsharingservice.dto.request.CarRequestDto;
+import com.example.carsharingservice.dto.response.CarResponseDto;
+import com.example.carsharingservice.mapper.RequestDtoMapper;
+import com.example.carsharingservice.mapper.ResponseDtoMapper;
+import com.example.carsharingservice.model.Car;
+import com.example.carsharingservice.service.CarService;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cars")
+@AllArgsConstructor
 public class CarController {
+    private final CarService carService;
+    private final ResponseDtoMapper<CarResponseDto, Car> responseDtoMapper;
+    private final RequestDtoMapper<CarRequestDto, Car> requestDtoMapper;
 
     @PostMapping
-    public String add() {
-        return "car was added";
+    public CarResponseDto add(@RequestBody CarRequestDto requestDto) {
+        Car car = carService.add(requestDtoMapper.mapToModel(requestDto));
+        return responseDtoMapper.mapToDto(car);
     }
 
     @GetMapping
-    public List<String> getAll() {
-        return List.of("get all cars");
+    public List<CarResponseDto> getAll() {
+        return carService.getAll().stream()
+                .map(responseDtoMapper::mapToDto)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public String get(@PathVariable Long id) {
-        return "get car with id " + id;
+    public CarResponseDto get(@PathVariable Long id) {
+        return responseDtoMapper.mapToDto(carService.get(id));
     }
 
     @PutMapping("/{id}")
-    public String update(@PathVariable Long id) {
-        return "car was updated with id " + id;
+    public CarResponseDto update(@PathVariable Long id,
+                                 @RequestBody @Valid CarRequestDto requestDto) {
+        Car car = requestDtoMapper.mapToModel(requestDto);
+        return responseDtoMapper.mapToDto(carService.update(id, car));
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable Long id) {
-        return "car was deleted with id " + id;
+    public void delete(@PathVariable Long id) {
+        carService.delete(id);
     }
 }
