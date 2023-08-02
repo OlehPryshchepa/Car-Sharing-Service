a57770355a9d1ecd12bac80ec6c90f3681a3e717
eugenetolkunov <yevhentolkunov@gmail.com>
2023-08-02 19:34:21 +0300

    [CS-24] added authentication controller (only registration)

--- src/main/java/com/example/carsharingservice/controller/AuthenticationController.java
+++ src/main/java/com/example/carsharingservice/controller/AuthenticationController.java
@@ -1,13 +1,24 @@
 package com.example.carsharingservice.controller;
 
+import com.example.carsharingservice.dto.request.UserRequestDto;
+import com.example.carsharingservice.dto.response.UserResponseDto;
+import com.example.carsharingservice.mapper.ResponseDtoMapper;
+import com.example.carsharingservice.model.User;
+import com.example.carsharingservice.service.AuthenticationService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class AuthenticationController {
+    private final AuthenticationService authService;
+    private final ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper;
 
     @PostMapping("/register")
-    public String register() {
-        return "user was registered";
+    public UserResponseDto register(@RequestBody UserRequestDto requestDto) {
+        User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
+        return userDtoResponseMapper.mapToDto(user);
     }
 }
