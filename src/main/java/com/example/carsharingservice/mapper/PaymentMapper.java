f19dc98ac5a3bfaf6b75e22af033dc4a8623a82b
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-02 12:53:46 +0300

    [CS-21] Removed redundant imports

--- src/main/java/com/example/carsharingservice/mapper/PaymentMapper.java
+++ src/main/java/com/example/carsharingservice/mapper/PaymentMapper.java
@@ -1,12 +1,8 @@
 package com.example.carsharingservice.mapper;
 
 import com.example.carsharingservice.config.MapperConfig;
-import com.example.carsharingservice.dto.request.PaymentRequestDto;
 import com.example.carsharingservice.dto.response.PaymentResponseDto;
-import com.example.carsharingservice.dto.response.UserResponseDto;
 import com.example.carsharingservice.model.Payment;
-import com.example.carsharingservice.model.User;
-import com.example.carsharingservice.service.RentalService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
