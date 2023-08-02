fa0d9518a9c64bec01c5e719479b26560b1d967c
Chris <tynkastya@gmail.com>
2023-08-02 17:52:17 +0300

    new solution

--- src/main/java/com/example/carsharingservice/controller/UserController.java
+++ src/main/java/com/example/carsharingservice/controller/UserController.java
@@ -2,7 +2,8 @@ package com.example.carsharingservice.controller;
 
 import com.example.carsharingservice.dto.request.UserRequestDto;
 import com.example.carsharingservice.dto.response.UserResponseDto;
-import com.example.carsharingservice.mapper.UserMapper;
+import com.example.carsharingservice.mapper.RequestDtoMapper;
+import com.example.carsharingservice.mapper.ResponseDtoMapper;
 import com.example.carsharingservice.model.User;
 import com.example.carsharingservice.service.UserService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -19,26 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final UserMapper userMapper;
+    private final ResponseDtoMapper<UserResponseDto, User> responseDtoMapper;
+    private final RequestDtoMapper<UserRequestDto, User> requestDtoMapper;
 
     @PutMapping("/{id}/role")
     public UserResponseDto update(@PathVariable Long id) {
         User user = userService.get(id);
         return user.getRole() == User.Role.CUSTOMER
-                ? userMapper.mapToDto(userService.update(id, User.Role.MANAGER))
-                : userMapper.mapToDto(userService.update(id, User.Role.CUSTOMER));
+                ? responseDtoMapper.mapToDto(userService.update(id, User.Role.MANAGER))
+                : responseDtoMapper.mapToDto(userService.update(id, User.Role.CUSTOMER));
     }
 
     @GetMapping("/me")
     public UserResponseDto get(Authentication authentication) {
-        return userMapper.mapToDto(userService.findByEmail(authentication.getName()));
+        return responseDtoMapper.mapToDto(userService.findByEmail(authentication.getName()));
     }
 
     @PutMapping("/me")
     public UserResponseDto updateProfile(Authentication authentication,
                                 @RequestBody UserRequestDto userRequestDto) {
-        User user = userMapper.mapToModel(userRequestDto);
+        User user = requestDtoMapper.mapToModel(userRequestDto);
         user.setId(userService.findByEmail(authentication.getName()).getId());
-        return userMapper.mapToDto(userService.update(user));
+        return responseDtoMapper.mapToDto(userService.update(user));
     }
 }
