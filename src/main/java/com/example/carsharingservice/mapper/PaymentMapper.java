0e7b14f230839640883b75617525a3774f39954c
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-02 12:51:41 +0300

    [CS-21] Added mappers

--- src/main/java/com/example/carsharingservice/mapper/PaymentMapper.java
+++ src/main/java/com/example/carsharingservice/mapper/PaymentMapper.java
@@ -3,17 +3,15 @@ package com.example.carsharingservice.mapper;
 import com.example.carsharingservice.config.MapperConfig;
 import com.example.carsharingservice.dto.request.PaymentRequestDto;
 import com.example.carsharingservice.dto.response.PaymentResponseDto;
+import com.example.carsharingservice.dto.response.UserResponseDto;
 import com.example.carsharingservice.model.Payment;
+import com.example.carsharingservice.model.User;
 import com.example.carsharingservice.service.RentalService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(config = MapperConfig.class, uses = RentalService.class)
+@Mapper(config = MapperConfig.class)
 public interface PaymentMapper {
-    @Mapping(target = "paymentAmount", source = "paymentAmount")
-    @Mapping(target = "rental", source = "rentalId")
-    Payment mapToModel(PaymentRequestDto dto);
-
     @Mapping(target = "paymentAmount", source = "paymentAmount")
     @Mapping(target = "rentalId", source = "payment.rental.id")
     PaymentResponseDto toDto(Payment payment);
---
+++ src/main/java/com/example/carsharingservice/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.carsharingservice.mapper;
+
+import com.example.carsharingservice.config.MapperConfig;
+import com.example.carsharingservice.dto.request.UserRequestDto;
+import com.example.carsharingservice.dto.response.UserResponseDto;
+import com.example.carsharingservice.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+    User mapToModel(UserRequestDto dto);
+    UserResponseDto mapToDto(User model);
+}
