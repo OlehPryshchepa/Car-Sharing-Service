6e55ddd014025b40b708e2f8febcf5e5fc1fd902
eugenetolkunov <yevhentolkunov@gmail.com>
2023-08-03 22:15:27 +0300

    [CS-48] fix rental repository

--- src/main/java/com/example/carsharingservice/repository/RentalRepository.java
+++ src/main/java/com/example/carsharingservice/repository/RentalRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,5 +16,9 @@ public interface RentalRepository extends JpaRepository<Rental, Long> {
 
     List<Rental> findByUserIdAndActualReturnDateIsNotNull(Long userId, PageRequest request);
 
+    @Query("FROM Rental r " +
+            "INNER JOIN FETCH r.car " +
+            "INNER JOIN FETCH r.user " +
+            "WHERE r.actualReturnDate=NULL AND r.returnDate < :localDateTime")
     List<Rental> findAllByActualReturnDateNullAndReturnDateLessThan(LocalDateTime localDateTime);
 }
