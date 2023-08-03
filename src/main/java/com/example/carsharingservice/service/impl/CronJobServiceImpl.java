d704e070bc797b551e13c7447b49cfcb50afc089
eugenetolkunov <yevhentolkunov@gmail.com>
2023-08-03 20:40:10 +0300

    [CS-48] added cron job

---
+++ src/main/java/com/example/carsharingservice/service/impl/CronJobServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.carsharingservice.service.impl;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import com.example.carsharingservice.model.Rental;
+import com.example.carsharingservice.service.NotificationService;
+import com.example.carsharingservice.service.RentalService;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableScheduling
+@AllArgsConstructor
+public class CronJobServiceImpl {
+    private RentalService rentalService;
+    private NotificationService notificationService;
+
+
+    @Scheduled(fixedDelay = 10000)
+    public void cronJobOverdueRentals() {
+        List<Rental> overdueRentals = rentalService.findOverdueRentals();
+        overdueRentals.stream()
+                .forEach(rental -> notificationService.sendTelegramMessage(rental.getUser(), getMessage(rental)));
+    }
+
+    private static String getMessage(Rental rental) {
+        LocalDateTime returnDate = rental.getReturnDate();
+        return "Your rental number: " + rental.getId() + "\n"
+                + "The model of the car you rented: " + rental.getCar().getModel() + "\n"
+                + "The brand of the car you rented: " + rental.getCar().getBrand() + "\n"
+                + "Lease start date: " + rental.getRentalDate() + "\n"
+                + "Lease end date: " + returnDate + "\n"
+                + "Days overdue: " + ChronoUnit.DAYS.between(returnDate, LocalDateTime.now());
+    }
+}
