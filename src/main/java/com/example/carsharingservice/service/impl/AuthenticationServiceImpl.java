9ffd06c00110bf8676c686560fd2e4d1ff9e0c4a
eugenetolkunov <yevhentolkunov@gmail.com>
2023-08-03 10:22:24 +0300

    [CS-40] added login method

--- src/main/java/com/example/carsharingservice/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/carsharingservice/service/impl/AuthenticationServiceImpl.java
@@ -1,15 +1,19 @@
 package com.example.carsharingservice.service.impl;
 
+import java.util.Optional;
+import com.example.carsharingservice.exception.AuthenticationException;
 import com.example.carsharingservice.model.User;
 import com.example.carsharingservice.service.AuthenticationService;
 import com.example.carsharingservice.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User register(String email, String firstName, String lastName, String password) {
@@ -21,5 +25,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         user.setRole(User.Role.CUSTOMER);
         user = userService.add(user);
         return user;
+    }
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isEmpty() || !passwordEncoder.matches(password, user.get().getPassword())) {
+            throw new AuthenticationException("Invalid email or password");
+        }
+        return user.get();
     }
 }
