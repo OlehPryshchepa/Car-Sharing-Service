1c890540ac6eca7a5c21b1edc82b1f31667fdc31
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-03 20:44:16 +0300

    fix

--- src/main/java/com/example/carsharingservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/carsharingservice/service/impl/UserServiceImpl.java
@@ -39,8 +39,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email).orElseThrow(
-                () -> new RuntimeException("Invalid email " + email));
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
